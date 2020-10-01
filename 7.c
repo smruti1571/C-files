f532494502512c420256c216ae9174122bd4367f
AnkanSwain <56729710+AnkanSwain@users.noreply.github.com>
2020-10-01 14:44:50 +0530

    Updated 7.c
    Added another method of swapping

--- 7.c
+++ 7.c
@@ -1,4 +1,5 @@
-//7.WAP to swap two numbers by using call by value and call by reference?
+//7.WAP to swap two numbers by using call by value and call by reference
+//  and using just two variables.
 #include<stdio.h>
 void swap_call_by_reference(int *a,int *b)
 {
@@ -14,6 +15,13 @@ void swap_call_by_value(int a,int b)
     b=temp;
 
 }
+void swap_with_two_var(int a,int b)
+{
+    a = a+b;
+    b = a-b;
+    a = a-b;
+}    
+    
 int main()
 {
     int n1,n2;
@@ -24,5 +32,7 @@ int main()
     printf("Vlues after swapping using call by reference n1 = %d & n2 = %d\n",n1,n2);
     swap_call_by_value(n1,n2);
     printf("Vlues after swapping using call by value n1 = %d & n2 = %d\n",n1,n2);
+    swap_with_two_var(n1,n2);
+    printf("Vlues after swapping using just two variables n1 = %d & n2 = %d\n",n1,n2);
     return 0;
 }
