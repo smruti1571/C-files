a603f03056900139f6e1d385d11e5595bc266e8e
Adyasha Naik <72915852+Adyasha05@users.noreply.github.com>
2020-10-15 15:02:39 +0530

    Add files via upload
    My contibution updated.

--- 5.c
+++ 5.c
@@ -1,48 +1,31 @@
-//5.WAP to multiply two matrices?
 #include<stdio.h>
 int main()
 {
-    int A[15][15],B[15][15],C[15][15],m,n,o,p;
-    printf("Enter the rows and columns of 1st matrix A : ");
-    scanf("%d %d",&m,&n);
-    printf("Enter the rows and columns of 2nd matrix B : ");
-    scanf("%d %d",&o,&p);
-    printf("Enter the elements of 1st matrix A : ");
-    for(int i=0;i<m;i++)
+    int a[50][50],tz=0,rows,columns;
+    printf("Enter the number of rows of Matrix : ");
+    scanf("%d",&rows);
+    printf("Enter the number of columns of Matrix : ");
+    scanf("%d",&columns);
+    printf("Enter the elements of Matrix : \n");
+    for(int i=0;i<rows;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<columns;j++)
         {
-            scanf("%d",&A[i][j]);
-        }
-    }
-    printf("Enter the elements of 2nd matrix B : ");
-    for(int k=0;k<o;k++)
-    {
-        for(int l=0;l<p;l++)
-        {
-            scanf("%d",&B[k][l]);
-        }
-    }
-    for(int q=0;q<m;q++)
-    {
-        for(int r=0;r<p;r++)
-        {
-            C[q][r]=0;
-            for(int s=0;s<p;s++)
+            scanf("%d",&a[i][j]);
+            if(a[i][j]==0)
             {
-                C[q][r]+=A[q][s]*B[s][r];
+                tz+=1;
             }
         }
     }
-    printf("Multiplication matrix of A Aand B , C = \n");
-    for(int t=0;t<m;t++)
+    if(tz>(rows*columns)/2)
     {
-        for(int u=0;u<p;u++)
-        {
-            printf(" %d ",C[t][u]);
-        }
-        printf("\n");
+        printf("The given Matrix is a Sparse Matrix.");
+    }
+    else{
+        printf("The given Matrix is not a Sparse Matrix.");
     }
     return 0;
-
 }
+
+
